45879b97cb18b494abc098f0f09f83108b54c629
ukjinlee <asllan77@gmail.com>
2019-02-19 10:57:48 -0500

    add columns to the time records model

--- db/migrate/20190219154635_create_time_records.rb
+++ db/migrate/20190219154635_create_time_records.rb
@@ -1,7 +1,8 @@
 class CreateTimeRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :time_records do |t|
-
+      t.string :username
+      t.integer :clock_type
       t.timestamps
     end
   end
